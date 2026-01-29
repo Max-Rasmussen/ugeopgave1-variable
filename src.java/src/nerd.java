b09c21baaeb3d06752c8a494d9861282d99c614a
Max <mara0027@stud.ek.dk>
2026-01-29 17:02:09 +0100

    Færdig med opgavesæt

---
+++ src.java/src/nerd.java
@@ -0,0 +1,235 @@
+
+
+
+public class nerd {
+
+    void main() {
+
+        System.out.println(" ");
+
+
+        //Opgave 1
+/*
+        double[] prices = {25.50, 15.00, 30.00};
+
+        double sumOfPrices = prices[0] + prices[1] + prices[2];
+
+        double taxPercent = 25.0;
+
+        double taxAmount = sumOfPrices * taxPercent / 100;
+
+        double finalPrice = sumOfPrices - taxAmount;
+
+        System.out.println(sumOfPrices);
+
+        System.out.println(finalPrice);
+*/
+
+
+
+        //Opgave 2
+/*
+        double foodPrice = 450.00;
+        double drinkPrice = 120.00;
+        double tip = 50.0;
+        int people = 4;
+
+double totalPrice =  foodPrice + drinkPrice + tip;
+
+int costPerPerson = (int) totalPrice / people;
+
+        System.out.println(costPerPerson);
+        */
+
+        //Opgave 3
+/*
+        double orginalPrice = 300.0;
+        double discountPercent = 20.00;
+        double discountAmount = orginalPrice * discountPercent / 100;
+        double discountedPrice = orginalPrice - discountAmount;
+
+        double taxPercent = 25.00;
+        double taxAmount = discountedPrice * taxPercent /100;
+        double finalCost = orginalPrice - discountAmount +  taxAmount;
+
+
+        System.out.println("Original pris: " + orginalPrice);
+        System.out.println("Beløb efter rabat: " + discountedPrice);
+        System.out.println("Beløb efter rabat og skat: " + finalCost);
+*/
+
+
+        //Opgave 4
+/*
+        int guests = 15;
+        int cupcakes = 40;
+
+        int cupcakesPerGuests = cupcakes / guests;
+
+        int leftoverCupcakes = cupcakes % guests;
+
+        System.out.println("each guests gets this many: " + cupcakesPerGuests);
+        System.out.println("leftover amount: " + leftoverCupcakes);
+        */
+
+
+        //Opgaver 5
+        /*
+StringBuilder bob = new StringBuilder();
+
+String name = "Anna";
+
+int age = 25;
+
+bob.append("Hello, ");
+bob.append(name);
+bob.append("!");
+bob.append(" You are ");
+bob.append(age);
+bob.append(" years old.");
+
+        System.out.println(bob);
+        */
+
+
+        //Opgave 6
+        /*
+        int score = 100;
+
+        System.out.println(score);
+
+        score += 50;
+
+        System.out.println(score);
+
+        score += 25;
+
+        System.out.println(score);
+*/
+
+        //Opgave 7
+/*
+        int hp = 150;
+
+        hp -= 25;
+
+        System.out.println(hp);
+
+        hp -= 30;
+
+        System.out.println(hp);
+        */
+
+
+        //Opgave 8
+        /*
+        double price = 50.00;
+
+        System.out.println(price);
+
+        price *= 2;
+
+        System.out.println(price);
+*/
+
+
+        //Opgave 9
+/*
+        int counter = 10;
+
+        System.out.println(counter);
+
+        counter++;
+
+        System.out.println(counter);
+
+        counter--;
+
+        System.out.println(counter);
+
+
+        */
+
+        //Opgave 10
+/*
+        int number = 10;
+
+        number += 5;
+
+        System.out.println(number);
+
+        number -= 3;
+
+        System.out.println(number);
+
+        number++;
+
+        System.out.println(number);
+
+        number--;
+
+        System.out.println(number);
+ */
+
+        //Opgave 11
+/*
+        String itemForSale = "Laptop";
+        double itemPrice =  7999.99;
+        boolean inStock = false;
+        int amountInStock = 15;
+        double discountPercent = 10.00;
+        String category = "Electronics";
+        */
+
+
+    //Opgave 12
+
+/*
+        String studentName = "Anna";
+        int testPoints = 85;
+        int assignmentPoints = 92;
+        double finalGrade = 88.5;
+        boolean passedTest = true;
+        */
+
+
+    //Opgave 13
+/*
+        String guestName = "Peter Hansen";
+        int roomNumber = 204;
+        int howManyNights = 3;
+        double costPerNight = 899.00;
+        boolean breakfastIncluded = true;
+        double totalPrice = 2697.00;
+*/
+
+
+        //Opgave 14
+/*
+        int stepsToday =8547;
+        int stepGoal = 10000;
+        double kilometerWalked = 6.2;
+        int caloriesBurnt = 420;
+        boolean goalReached = false;
+        */
+
+
+        //Opgave 15
+/*
+        String bookTitle;
+        String authorName;
+        String isbnNumber;
+        int amountOfPages;
+        boolean canBeBorrowed;
+        String whoBorrowed;
+        String deadlineForReturn;
+        double fineForNoReturn;
+        boolean isBestSeller;
+        double bookRating;
+        */
+
+
+
+
+    }
+}
